c4785109a391c4c63029ddcc258a437f8401ee1a
NicholasLau97 <46722517+NicholasLau97@users.noreply.github.com>
2022-03-26 15:44:29 +0800

    API CALL success

--- ImageCache/app/src/main/java/com/example/imagecache/api/SearchImagesAPI.kt
+++ ImageCache/app/src/main/java/com/example/imagecache/api/SearchImagesAPI.kt
@@ -1,14 +1,21 @@
 package com.example.imagecache.api
 
 import com.example.imagecache.models.SearchImagesResponse
+import com.example.imagecache.util.Constants.Companion.API_HOST
+import com.example.imagecache.util.Constants.Companion.API_KEY
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface SearchImagesAPI {
 
     @GET("ImageSearchAPI")
     suspend fun searchImages(
+        @Header("X-RapidAPI-Host")
+        host:String = API_HOST,
+        @Header("X-RapidAPI-Key")
+        host_key:String = API_KEY,
         @Query("q")
         q: String = "taylor swift",
         @Query("pageNumber")
--- ImageCache/app/src/main/java/com/example/imagecache/repository/SearchImagesRepository.kt
+++ ImageCache/app/src/main/java/com/example/imagecache/repository/SearchImagesRepository.kt
@@ -1,8 +1,10 @@
 package com.example.imagecache.repository
 
 import com.example.imagecache.api.RetrofitInstance
+import com.example.imagecache.util.Constants.Companion.API_HOST
+import com.example.imagecache.util.Constants.Companion.API_KEY
 
 class SearchImagesRepository {
     suspend fun searchImages(q:String) =
-        RetrofitInstance.api.searchImages(q,"1","10","true")
+        RetrofitInstance.api.searchImages(API_HOST, API_KEY,q,"1","10","true")
 }
