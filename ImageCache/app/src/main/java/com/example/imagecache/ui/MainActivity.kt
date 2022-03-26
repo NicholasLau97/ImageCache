decdf3f61df9964620f42c7900f7396f52d9ae61
NicholasLau97 <46722517+NicholasLau97@users.noreply.github.com>
2022-03-26 13:10:12 +0800

    recreate

--- ImageCache/app/src/main/java/com/example/imagecache/ui/MainActivity.kt
+++ ImageCache/app/src/main/java/com/example/imagecache/ui/MainActivity.kt
@@ -2,37 +2,13 @@ package com.example.imagecache.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import com.example.imagecache.R
-import com.example.imagecache.databinding.ActivityMainBinding
-import com.example.imagecache.repository.ImagesRepository
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
-        val imagesRepository = ImagesRepository()
-        val viewModelProviderFactory = MainViewModelProviderFactory(application,imagesRepository)
-        val viewModel= ViewModelProvider(this,viewModelProviderFactory).get(MainViewModel::class.java)
-        binding.viewmodel = viewModel
 
     }
 
 
-
-
-=======
-import com.example.imagecache.R
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
->>>>>>> parent of a6ab4ad (Intial settings)
 }
--- ImageCache/app/src/main/java/com/example/imagecache/ui/MainViewModel.kt
+++ ImageCache/app/src/main/java/com/example/imagecache/ui/MainViewModel.kt
@@ -3,59 +3,22 @@ package com.example.imagecache.ui
 import android.app.Application
 import android.view.View
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
-import com.example.imagecache.models.ImagesResponse
-import com.example.imagecache.models.Value
-import com.example.imagecache.repository.ImagesRepository
-import com.example.imagecache.util.Resource
-import kotlinx.coroutines.launch
-import retrofit2.Response
+import androidx.lifecycle.ViewModel
 
-class MainViewModel(app:Application,val repository: ImagesRepository):AndroidViewModel(app) {
+class MainViewModel():ViewModel() {
 
     var searchText:String?=null
     var urlWebView:String?=null
 
 
-    val myResponse:MutableLiveData<ImagesResponse> = MutableLiveData()
-
-    val searchImages:MutableLiveData<Resource<ImagesResponse>> = MutableLiveData()
-    var searchImagesResponse:ImagesResponse?=null
 
     fun onSearchBtnClick(view: View){
 
-        searchImages(searchText)
 
 
     }
     fun onWebViewBtnClick(view:View){
 
     }
-    fun searchImages(searchText:String?){
 
-    }
-    fun displayImages(images:List<Value>){
-        //display the images to infinite recycler view
-    }
-
-    suspend fun searchImagesByTextFromAPI(searchText: String){
-        searchImages.postValue(Resource.Loading())
-        val response = repository.searchImages(searchText,"1","10","true")
-        searchImages.postValue(handleSearchImagesResponse(response))
-    }
-    fun handleSearchImagesResponse(response: Response<ImagesResponse>):Resource<ImagesResponse>{
-        if(response.isSuccessful){
-            response.body()?.let{resultResponse->
-                if(searchImagesResponse == null){
-                    searchImagesResponse = resultResponse
-                }else{
-                    val newImages = searchImagesResponse?.value
-                }
-                return Resource.Success(searchImagesResponse?:resultResponse)
-
-            }
-        }
-        return Resource.Error(response.message())
-    }
 }
